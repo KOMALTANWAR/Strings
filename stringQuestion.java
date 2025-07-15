d83e21e15611830f2567845327f1db1e95c420f9
vampire <komaltanwar168@gmail.com>
2025-07-15 21:29:05 +0530

    Shortest path question added

--- stringQuestion.java
+++ stringQuestion.java
@@ -10,7 +10,29 @@ public class stringQuestion{
        }
        return true;
     }
+    //Question:Given a route containing four directions(N,E,W,S) .Find the shortest path to
+    // reach the destination "WNEENESENNN".
+    public static int findShortestPath(String destination){
+      int x=0;
+      int y=0;
+      int length=destination.length();
+      for(int i=0;i<length;i++){
+         if(destination.charAt(i)=='E'){
+          x++;
+         }
+         else if(destination.charAt(i)=='W'){
+          x--;
+         }
+         else if(destination.charAt(i)=='N'){
+          y++;
+         }else{
+          y--;
+         }
+      }
+      return (int)Math.sqrt(y*y+x*x);
+    }
     public static void main(String args[]){
      System.out.println( isPalindrome("racecar"));
+     System.out.println(findShortestPath("WNEENESENNN"));
     }
 }
